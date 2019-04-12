dd633f2ff1d1e74639774f683371d24905052e20
MGenteluci <mgenteluci97@gmail.com>
2019-04-11 23:42:42 -0300

    Add health route

---
+++ app/routes/author.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => res.status(200).json({
+    message: 'Hey, I\'m glad you found this one! My name is Matheus Genteluci!'
+}));
--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const authorRoute = require('./app/routes/author');
+const healthRoute = require('./app/routes/health');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/author', (req, res) => {
-  return res.status(200).json({
-    message: 'Hey, I\'m glad you found this one! My name is Matheus Genteluci!'
-  });
-});
+app.get('/author', authorRoute);
+app.get('/health', healthRoute);
 
 app.get('/', (req, res) => {
   return res.status(200).json({
