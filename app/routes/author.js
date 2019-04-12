355d9ff4dcfd099e894dcb051ba8522cd48ac317
MGenteluci <mgenteluci97@gmail.com>
2019-04-11 23:53:27 -0300

    export author and health routes

--- app/routes/author.js
+++ app/routes/author.js
@@ -3,3 +3,5 @@ const router = require('express').Router();
 router.get('/', (req, res) => res.status(200).json({
     message: 'Hey, I\'m glad you found this one! My name is Matheus Genteluci!'
 }));
+
+module.exports = router;
